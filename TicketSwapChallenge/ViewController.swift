3174613713634c7459832265efb8616ca6a93f05
Fernando Perez <fdoiv7@gmail.com>
2022-07-14 20:37:31 -0500

    Initial Commit

---
+++ TicketSwapChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TicketSwapChallenge
+//
+//  Created by Fernando Ives on 14/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
